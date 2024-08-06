f7649dbbf6ceb732332584da5a264245a772dbb6
Richard Mauricio Soria Asazan <151602207+RichardSoria@users.noreply.github.com>
2024-08-06 09:33:46 -0500

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Despliegue De Proyecto de REACT en VERCEL</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
